5fa1a8e566f1a21eb00c4640a0e509a30479adc0
ForkBug <qfhere@gmail.com>
2022-09-08 20:42:11 +0800

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiIOSVersion;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
